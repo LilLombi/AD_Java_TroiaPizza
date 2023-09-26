ae2dc8527571221e02575d5f5f7463da414c0a97
Daniel <danicharanga18@gmail.com>
2023-09-26 19:53:32 +0200

    He modificado SES

--- src/main/java/com/mycompany/pruebagit3/PruebaGit3.java
+++ src/main/java/com/mycompany/pruebagit3/PruebaGit3.java
@@ -12,6 +12,6 @@ package com.mycompany.pruebagit3;
 public class PruebaGit3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("CHUCHA, ME GOLPEARON EN LOS MONDONGOS");
     }
 }
