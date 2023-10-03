35200c3d3cf304c142d850e8929be434554a2e7e
Daniel <danicharanga18@gmail.com>
2023-10-03 20:44:48 +0200

    Añadido Enum Ingrediente

--- src/main/java/com/mycompany/troiapizza/Ingrediente.java
+++ src/main/java/com/mycompany/troiapizza/Ingrediente.java
@@ -13,7 +13,7 @@ public enum Ingrediente {
     SBBQ("Salsa BBQ", 01), SBOU("Salsa Bourbon", 02), STOM("Salsa de tomate", 03), SCREM("Salsa crema", 04),
     CYRK("Jamón York", 11), CBCN("Bacon", 12), CPPK("Pulled Pork", 13), CPPP("Pepperoni", 14), CNCH("Anchoas", 15), CATN("Atún", 16),
     VCHM("Champiñones", 21), VACT("Aceitunas", 22), VCEB("Cebolla", 23), VCCR("Cebolla caramelizada", 24), VTOM("Tomate", 25),
-        VPIV("Pimiento verde", 26), VPIR("Pimiento rojo", 27), VPNA("Piña", 28), VMIZ("Maiz", 29),
+    VPIV("Pimiento verde", 26), VPIR("Pimiento rojo", 27), VPNA("Piña", 28), VMIZ("Maiz", 29),
     QPAR("Queso parmesano", 31), QMZZ("Queso mozzarela", 32), QCDD("Queso cheddar", 33), QAZL("Queso azul", 34), QEDM("Queso edam", 35);
     
     
